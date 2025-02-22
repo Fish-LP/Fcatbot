14419f2d4ec022a001acecfed921e4f92e9584c0
Fish-LP <fish.zh@outlook.com>
2025-02-22 19:16:55 +0800

    ✨ 从配置文件加载配置，优化配置管理逻辑

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
-EVENT_QUEUE_MAX_SIZE = 64  # 事件队列最大长度
-PLUGINS_DIR = "./plugins"  # 插件目录
-META_CONFIG_PATH = None  # 元数据，所有插件一份(只读)
-PERSISTENT_DIR = "./data"  # 插件私有数据目录
+from .utils import UniversalLoader
 
-OFFICIAL_GROUP_MESSAGE_EVENT = 'napcat.group'
-OFFICIAL_PRIVATE_MESSAGE_EVENT = 'napcat.private'
-OFFICIAL_REQUEST_EVENT = 'napcat.request'
-OFFICIAL_NOTICE_EVENT = 'napcat.notice'
+# 读取配置文件
+def load_config(config_path="config.yaml"):
+    with UniversalLoader(config_path) as f:
+        return f.load(f)
+
+# 加载配置
+config: UniversalLoader = load_config()
+
+# 使用配置
+EVENT_QUEUE_MAX_SIZE = config.get("EVENT_QUEUE_MAX_SIZE", 64)  # 事件队列最大长度
+PLUGINS_DIR = config.get("PLUGINS_DIR", "./plugins")  # 插件目录
+META_CONFIG_PATH = config.get("META_CONFIG_PATH", None)  # 元数据，所有插件一份(只读)
+PERSISTENT_DIR = config.get("PERSISTENT_DIR", "./data")  # 插件私有数据目录
+
+OFFICIAL_GROUP_MESSAGE_EVENT = config.get("OFFICIAL_GROUP_MESSAGE_EVENT", 'meta.bot.group')
+OFFICIAL_PRIVATE_MESSAGE_EVENT = config.get("OFFICIAL_PRIVATE_MESSAGE_EVENT", 'meta.bot.private')
+OFFICIAL_REQUEST_EVENT = config.get("OFFICIAL_REQUEST_EVENT", 'meta.bot.request')
+OFFICIAL_NOTICE_EVENT = config.get("OFFICIAL_NOTICE_EVENT", 'meta.bot.notice')
