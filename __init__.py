83fa3b258991d14a4af85ed3be6e14fce3a35e88
Fish-LP <fish.zh@outlook.com>
2025-02-23 17:53:04 +0800

    ✨ 更新版本号格式说明并修正setup.py中的依赖项拼写错误

--- __init__.py
+++ __init__.py
@@ -2,20 +2,10 @@
 # @Author       : Fish-LP fish.zh@outlook.com
 # @Date         : 2025-02-12 12:38:40
 # @LastEditors  : Fish-LP fish.zh@outlook.com
-# @LastEditTime : 2025-02-22 01:22:55
+# @LastEditTime : 2025-02-23 17:26:58
 # @Description  : 喵喵喵, 我还没想好怎么介绍文件喵
 # @Copyright (c) 2025 by Fish-LP, MIT License 
 # -------------------------
-from .setup import __version__
-# 版本号的格式为：主版本号.次版本号.修订号（MAJOR.MINOR.PATCH），其中每个部分都是非负整数，且禁止在数字前补零。
-# 主版本号（MAJOR） ：表示软件的重大变更，通常涉及不兼容的 API 修改、重大功能新增或旧功能的废弃。当主版本号增加时，次版本号和修订号必须归零。
-# 次版本号（MINOR） ：表示向后兼容的功能新增或改进。当次版本号增加时，修订号必须归零。
-# 修订号（PATCH） ：表示向后兼容的问题修正或小的改进。
-# 每次提交前请视情况修改版本号
-# dev
-# alpha
-# bate
-# release
 from .client import BotClient
 from .utils import get_log, UniversalDataIO
 from .client import BotClient
--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@
 # @Author       : Fish-LP fish.zh@outlook.com
 # @Date         : 2025-02-16 12:50:14
 # @LastEditors  : Fish-LP fish.zh@outlook.com
-# @LastEditTime : 2025-02-22 19:26:49
+# @LastEditTime : 2025-02-23 17:39:58
 # @Description  : 喵喵喵, 我还没想好怎么介绍文件喵
 # @Copyright (c) 2025 by Fish-LP, MIT License 
 # -------------------------
 from setuptools import setup, find_packages 
-__version__ = '0.0.2-dev'
 setup(
     name='Fbot',  # 包名称
-    version=__version__,  # 版本号
+    version='0.0.2-dev',  # 版本号
+    # 版本号的格式为：主版本号.次版本号.修订号（MAJOR.MINOR.PATCH），其中每个部分都是非负整数，且禁止在数字前补零。
+    # 主版本号（MAJOR） ：表示软件的重大变更，通常涉及不兼容的 API 修改、重大功能新增或旧功能的废弃。当主版本号增加时，次版本号和修订号必须归零。
+    # 次版本号（MINOR） ：表示向后兼容的功能新增或改进。当次版本号增加时，修订号必须归零。
+    # 修订号（PATCH） ：表示向后兼容的问题修正或小的改进。
+    # 每次提交前请视情况修改版本号
+    # dev alpha bate release
     description='基于ncatbot开发',  # 简短描述
     author='Fish-LP',  # 作者姓名
     author_email='fish.zh@outlook.com',  # 作者邮箱
@@ -21,7 +26,7 @@ setup(
         'aifile',
         'aiohttp',
         'websockets',
-        'tdqm',
+        'tqdm',
         'packaging',
     ],
     classifiers=[  # 分类器
